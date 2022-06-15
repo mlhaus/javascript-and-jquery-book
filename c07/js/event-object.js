db7697f712d7c50ca1e8ee50e0271ebde5db7e56
seanrickard <sean.rickard26@gmail.com>
2022-06-14 23:24:58 -0400

    fix so date shows accurately

--- c07/js/event-object.js
+++ c07/js/event-object.js
@@ -3,7 +3,7 @@ $(function() {
   $('li').on('click', function(e) {
     $('li span').remove();
     var date = new Date();
-    date.setTime(e.timeStamp);
+    date.setTime(date - e.timeStamp);
     var clicked = date.toDateString();
     $(this).append('<span class="date">' + clicked + ' ' + e.type + '</span>');
   });
